b19ab2aace62ed08acacb5888f342c32c91246f8
noelanthonyraj <noelanthonyraj@gmail.com>
2025-04-18 23:05:31 +0800

    Update admin.py

--- myhealth/admin.py
+++ myhealth/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Userdata, Qualification, MedicalHistory, Share
 
-# Register your models here.
+# Register your models here for testing.
 admin.site.register(Userdata)
 admin.site.register(Qualification)
 admin.site.register(MedicalHistory)
